d698a918d2675d22d908b44d71b6e87b781117e9
Buddhi Kavindra <visitbudkavin@gmail.com>
2020-01-22 18:44:48 +0530

    Version 0.1.0

--- studio/buddhi.js
+++ studio/buddhi.js
@@ -1,68 +1,202 @@
 //Magenta models initializing
-const musicEngine = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/multitrack_chords');
-const modelEngine = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small');
+/* const musicEngine = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/multitrack_chords');
+const modelEngine = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small'); */
 
-/* const musicEngine = new mm.MusicVAE('checkpoints/multitrack_chords');
-const modelEngine = new mm.MusicVAE('checkpoints/mel_2bar_small'); */
+const musicEngine = new mm.MusicVAE('checkpoints/multitrack_chords');
+const modelEngine = new mm.MusicVAE('checkpoints/mel_2bar_small');
 
 //Initializing Elements Variables
-const chordProgSelector = document.querySelector('#bl-chordprogression');
+const chordProgSelector = document.querySelectorAll('.chords');
 const statusSpinner = document.querySelector('#bl-spinner');
 const statusText = document.querySelector('.status-text');
 const btnShuffle = document.querySelector('#btnShuffle');
 const btnGenerate = document.querySelector('#btnGenerate');
 const btnPlay = document.querySelector('#btnPlay');
 const btnSave = document.querySelector('#btnSave');
+const btnMerge = document.querySelector('#btnMerge');
+const btnProceed = document.querySelector('#btnProceed');
+const btnNew = document.querySelector('#btnNew');
+const stepSections = document.querySelectorAll('.bl-studio--controllers');
 
 //Global Variables
 let encodedMusic;
 let genSeq = [];
 let generatedMusic;
 let playSample;
-let downloadSample;
 let randomNum = 1;
-let theChords = ["D", "Bm", "G", "A"];
+let theChords = [];
 const audioPlayer = initPlayerAndEffects();
 const melodySamplesArray = [Melody1, Melody2, Melody3, Melody4, Melody5];
 let melodySample = Melody1;
-const chordProgressionsArray = [
-    ['C, Am, F, G', 0],
-    ['D, Bm, G, A', 1],
-    ['F, Dm, Bb, C', 2],
-    ['G, Am, C, D', 3]
-];
 
-const chordProgressions = [
-    ['C', 'Am', 'F', 'G'],
-    ['D', 'Bm', 'G', 'A'],
-    ['F', 'Dm', 'Bb', 'C'],
-    ['G', 'Am', 'C', 'D']
-]
+const chordProgressions = ['F', 'Dm', 'Bb', 'C'];
+
+//assign onclick functions to butons
+btnProceed.onclick = getChordProgression;
+btnShuffle.onclick = generateSample;
+btnGenerate.onclick = generateMultiChords;
+btnMerge.onclick = mergeSequences;
+btnPlay.onclick = playSequence;
+btnSave.onclick = saveSequence;
+btnNew.onclick = reinitializeTool;
 
 Promise.all([musicEngine.initialize(), modelEngine.initialize()]).then(() => {
     console.log('Loaded!');
     statusSpinner.style.visibility = "hidden";
     statusText.innerHTML = "Ready!";
     statusText.classList.remove('processing');
-    btnShuffle.disabled = false;
-    btnGenerate.disabled = false;
+    btnProceed.disabled = false;
+    chordProgSelector.forEach((elmnt) => {
+        elmnt.disabled = false;
+    });
 });
 
 /* 
 Here goes the main functions
  */
 
-//Load chord progressions to the dropdown menu
-chordProgSelector.setOptions(chordProgressionsArray);
+//Load chord default progressions at initialization
+function setChordProgression() {
+    chordProgSelector.forEach((elmnt, indx) => {
+        elmnt.value = chordProgressions[indx];
+        elmnt.disabled = true;
+    });
+}
 
-//shuffle melody samples
-btnShuffle.addEventListener('click', () => {
+//Get chord progressions
+function getChordProgression() {
+    theChords = [];
+    chordProgSelector.forEach((elmnt, indx) => {
+        try {
+            mm.chords.ChordSymbols.pitches(elmnt.value);
+            theChords.push(elmnt.value);
+        } catch (error) {
+            document.querySelector('.step-status-text1').innerHTML = 'Invalid Chord: "' + elmnt.value + '"';
+        }
+    });
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 4; j++) {
+            theChords.push(theChords[j]);
+        }
+    }
+
+    if (theChords.length == 40) {
+        document.querySelector('.step-status-text1').innerHTML = 'Chord Progressions Set!'
+        stepSections[1].classList.remove('disable-step');
+        stepSections[1].scrollIntoView();
+        btnShuffle.disabled = false;
+        stepSections[0].classList.add('disable-step');
+        btnProceed.disabled = true;
+    }
+}
+
+//Generate sample
+function generateSample() {
     randomNum = Math.floor((Math.random() * 5));
     melodySample = melodySamplesArray[randomNum];
-});
+    try {
+        modelEngine.encode([melodySample]).then(sample => {
+            console.log("Encoding", sample);
+            encodedMusic = sample;
+        }).then(() => {
+            document.querySelector('.step-status-text2').innerHTML = 'Sample Generated!'
+            stepSections[2].classList.remove('disable-step');
+            stepSections[2].scrollIntoView();
+            btnGenerate.disabled = false;
+            stepSections[1].classList.add('disable-step');
+            btnShuffle.disabled = true;
+        });
+    } catch (error) {
+        document.querySelector('.step-status-text2').innerHTML = 'Oops something went wrong. Error: ' + error;
+    }
+
+}
+
+/* 
+This is the main function that generates melody
+ */
+async function generateMultiChords() {
+    genSeq = [];
+    try {
+        await theChords.map((chords, index) => {
+            console.log(chords + " at " + index);
+            musicEngine.decode(encodedMusic, 1, [chords], 24).then(sample => {
+                console.log("Done", sample);
+                genSeq.push(sample[0]);
+            });
+        });
+
+        document.querySelector('.step-status-text3').innerHTML = 'Music Sequences Generated!'
+        stepSections[3].classList.remove('disable-step');
+        stepSections[3].scrollIntoView();
+        btnMerge.disabled = false;
+        stepSections[2].classList.add('disable-step');
+        btnGenerate.disabled = true;
+    } catch (error) {
+        document.querySelector('.step-status-text3').innerHTML = 'Oops something went wrong. Error: ' + error;
+    }
+
+}
+
+//concatenate Generated Samples
+async function mergeSequences() {
+    try {
+        const tempseq = mm.sequences.clone(genSeq[0]);
+        let numSteps = genSeq[0].totalQuantizedSteps;
+        for (let i = 1; i < genSeq.length; i++) {
+            const seqZ = mm.sequences.clone(genSeq[i]);
+            seqZ.notes.forEach((note, index) => {
+                if (note.pitch != 0) {
+                    note.quantizedStartStep += numSteps;
+                    note.quantizedEndStep += numSteps;
+                    tempseq.notes.push(note);
+                } else {
+                    console.log(note.pitch + " ignored!");
+                }
+
+            });
+            numSteps += seqZ.totalQuantizedSteps;
+        }
+        tempseq.totalQuantizedSteps = numSteps;
+
+        playSample = tempseq;
+
+        document.querySelector('.step-status-text4').innerHTML = 'Sequences Merged Successfully!'
+        stepSections[4].classList.remove('disable-step');
+        stepSections[4].scrollIntoView();
+        btnPlay.disabled = false;
+        stepSections[3].classList.add('disable-step');
+        btnMerge.disabled = true;
+
+    } catch (error) {
+        document.querySelector('.step-status-text4').innerHTML = 'Oops something went wrong. Error: ' + error;
+    }
+
+}
+
+//Refining Sequences
+function refineSequences(sequenz) {
+    const instrumentSeq = mm.sequences.mergeInstruments(sequenz);
+    const unquantizedSeq = mm.sequences.unquantizeSequence(instrumentSeq);
+    unquantizedSeq.ticksPerQuarter = 24;
+    return unquantizedSeq;
+}
+
+//Play Generated Melody
+function playSequence() {
+    document.querySelector('.step-status-text5').innerHTML = 'Playing!'
+    audioPlayer.start(playSample).then(() => {
+        document.querySelector('.step-status-text5').innerHTML = 'Played!'
+        stepSections[5].classList.remove('disable-step');
+        stepSections[5].scrollIntoView();
+        btnSave.disabled = false;
+        btnNew.disabled = false;
+    });
+}
 
 //Save Generated Melody as MIDI
-btnSave.addEventListener('click', () => {
+function saveSequence() {
     try {
         const midiB = mm.sequenceProtoToMidi(playSample);
         const fileB = new Blob([midiB], { type: 'audio/midi' });
@@ -73,63 +207,30 @@ btnSave.addEventListener('click', () => {
         //a.innerHTML = "Download";
         a.download = 'buddhilive_' + Date.now() + '.mid';
         a.click();
-
+        document.querySelector('.step-status-text6').innerHTML = "Downloaded!";
     } catch (error) {
-        statusText.innerHTML = "Oops! Something went wrong! Try generating again. Error: " + error;
+        document.querySelector('.step-status-text6').innerHTML = "Oops! Something went wrong! Try generating again. Error: " + error;
         btnSave.disabled = true;
     }
+}
 
-});
-
-//Play Generated Melody
-btnPlay.addEventListener('click', () => {
-    audioPlayer.start(playSample);
-});
-
-/* 
-This is the main function that generates melody
- */
-btnGenerate.addEventListener('click', async() => {
-
-    let chordIndex = parseInt(chordProgSelector.getValue());
-    console.log(chordIndex, typeof chordIndex);
-    if (chordIndex >= 0) {
-        theChords = chordProgressions[chordIndex];
+//Reinitialize
+function reinitializeTool() {
+    for (let i = 1; i <= 6; i++) {
+        document.querySelector('.step-status-text' + i).innerHTML = "";
     }
-
-    statusText.innerHTML = "Sample " + randomNum + " Encoding...";
-    statusText.classList.add('processing');
-
-    await modelEngine.encode([melodySample]).then(sample => {
-        console.log("Namo Buddhaya!", sample);
-        statusText.innerHTML = "Latent Vector Generated..."
-        encodedMusic = sample;
+    stepSections.forEach((elmnt, index) => {
+        if (index == 4 || index == 5) {
+            elmnt.classList.add('disable-step');
+        }
     });
+    btnProceed.disabled = false;
+    btnPlay.disabled = true;
+    btnSave.disabled = true;
+    btnNew.disabled = true;
 
-    genSeq = [];
-
-    theChords.map(async(chords) => {
-        console.log(chords);
-
-        statusText.innerHTML = "Melody Sample is Generating for " + chords + " Chord...";
-
-        Promise.all([musicEngine.decode(encodedMusic, 1, [chords], 24).then(sample => {
-            console.log("done", sample);
-            genSeq.push(sample[0]);
-        })]).then(() => {
-            downloadSample = playSample;
-            playSample = concatenateSequences(genSeq);
-        });
-    });
-
-
-
-    statusText.innerHTML = "Melody Generation Completed!";
-    statusText.classList.remove('processing');
-
-    btnPlay.disabled = false;
-    btnSave.disabled = false;
-});
+    stepSections[0].scrollIntoView();
+}
 
 /* 
 These functions are taken from Magenta Example
@@ -178,26 +279,6 @@ function initPlayerAndEffects() {
     return player;
 }
 
-//concatenate Generated Samples
-function concatenateSequences(seqs) {
-    try {
-        const seq = mm.sequences.clone(seqs[0]);
-        let numSteps = seqs[0].totalQuantizedSteps;
-        for (let i = 1; i < seqs.length; i++) {
-            const s = mm.sequences.clone(seqs[i]);
-            s.notes.forEach(note => {
-                note.quantizedStartStep += numSteps;
-                note.quantizedEndStep += numSteps;
-                seq.notes.push(note);
-            });
-            numSteps += s.totalQuantizedSteps;
-        }
-        seq.totalQuantizedSteps = numSteps;
-
-        return seq;
-
-    } catch (error) {
-        statusText.innerHTML = "Oops! Something went wrong! Try generating again. Error: " + error;
-    }
-
+window.onload = () => {
+    setChordProgression();
 }
