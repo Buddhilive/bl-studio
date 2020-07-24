a1074696cbd1258a05b4f5f642a0f8fdc513fbc8
Buddhi Kavindra <visitbudkavin@gmail.com>
2020-07-25 00:55:23 +0530

    fixing

--- src/app/shared/global.variables.ts
+++
@@ -1,4 +0,0 @@
-import { environment } from '../../environments/environment';
-export class GlobalVariables {
-  MODEL_IMPROV_RNN = environment.improv_rnn;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  improv_rnn: 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
-};
